75f0530af89eee035a9a319894d0a2bb86e46cd7
Alome <astrocoder007@gmail.com>
2020-05-09 09:09:46 -0700

    Added stuffs

--- app/src/main/java/com/resulvesdk/MainActivity.java
+++ app/src/main/java/com/resulvesdk/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.resulve.sdk.soundPay;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new soundPay().withReceive(this,"1234");
     }
 }
--- resulve/src/main/java/com/resulve/sdk/soundPayRecieve.java
+++ resulve/src/main/java/com/resulve/sdk/soundPayRecieve.java
@@ -1,15 +1,19 @@
 package com.resulve.sdk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class soundPayRecieve extends AppCompatActivity {
-    com.resulve.sdk.soundPay soundPay;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
       //  soundPay.withReceive(this,"");
+        Intent i=getIntent();
+        String s=i.getStringExtra("receiverId");
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
