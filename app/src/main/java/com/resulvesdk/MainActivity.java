bac61068a2b037b7a2c5a3069eeb5c1057545221
Alome <astrocoder007@gmail.com>
2020-05-09 09:18:21 -0700

    Added stuffs

--- app/src/main/java/com/resulvesdk/MainActivity.java
+++ app/src/main/java/com/resulvesdk/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new soundPay().withReceive(this,"1234");
+        new soundPay().withReceive(this,"theNameOfAlomeDanielISAlomeOchoche");
     }
 }
